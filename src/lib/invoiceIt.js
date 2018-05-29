57bbc9675560ed834da0b9fe6bf3ad630ebf0440
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-05-29 15:05:59 +0200

    feat(lib/invoiceIt.js) export customized instance

---
+++ src/lib/invoiceIt.js
@@ -0,0 +1,21 @@
+import invoiceIt from '@rimiti/invoice-it';
+
+// Configuration
+invoiceIt.configure({
+    emitter: {
+        name: 'Dim Solution',
+        street_number: '15',
+        street_name: 'Rue Jean Jaures',
+        zip_code: '75012',
+        city: 'Paris',
+        country: 'France',
+        phone: '01 00 00 00 00',
+        mail: 'contact@dimsolution.com',
+        website: 'www.dimsolution.com',
+    },
+    global: {
+        logo: 'http://placehold.it/230x70&text=Dim+Solution',
+    },
+});
+
+export default invoiceIt;
